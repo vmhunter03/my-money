a6df47f65717f6422a0a17a3be2b29304ca7b302
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-06-07 09:35:58 +0800

    adding Signup Template

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,49 @@
-import styles from './Signup.module.css'
+import { useState } from "react";
 
-import React from 'react'
+//styles
+import styles from "./Signup.module.css";
+
+import React from "react";
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
   return (
-    <div>Signup</div>
-  )
+    <form onSubmit={handleSubmit} className={styles[`signup-form`]}>
+      <h2>Sign up</h2>
+      <label>
+        <span>Email:</span>
+        <input
+          placeholder="email"
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          placeholder="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Display Name:</span>
+        <input
+          placeholder="name"
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <button className="btn">Create Account</button>
+    </form>
+  );
 }
