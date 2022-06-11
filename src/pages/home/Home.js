a25d8f594f4fea749cb8a9abd8d5c60ff20a864c
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-06-11 16:12:10 +0800

    Listing Transactions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,23 @@
 // hooks
-import TransactionForm from "./TransactionForm";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+
+// components
+import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
+
 // styles
 import styles from "./Home.module.css";
 
 export default function Home() {
   const { user } = useAuthContext();
+  const { documents, error } = useCollection("transactions");
   return (
     <div className={styles.container}>
-      <div className={styles.content}>Transaction List</div>
+      <div className={styles.content}>
+        {error && <p>{error}</p>}
+        {documents && <TransactionList transactions={documents} />}
+      </div>
       <div className={styles.sidebar}>
         <TransactionForm uid={user.uid} />
       </div>
