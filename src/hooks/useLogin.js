a41abca7bfd8147827c29a7d9dfc39e15f8319e1
Halid_Macalangcom <vmhunterjob@gmail.com>
2022-06-11 08:18:02 +0800

    Created a Custome Login Hook

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -2,31 +2,23 @@ import { useState, useEffect } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
-export const useSignup = () => {
+export const useLogin = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
-  const signup = async (email, password, displayName) => {
+
+  const login = async (email, password) => {
     setError(null);
     setIsPending(true);
 
+    // sign the user out
     try {
-      // signup user
-      const res = await projectAuth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
+      const res = await projectAuth.signInWithEmailAndPassword(email, password);
 
-      if (!res) {
-        throw new Error("Could not complete signup");
-      }
-
-      // add display name to user
-      await res.user.updateProfile({ displayName });
-
-      //dispatch login action
+      //dispatch logout action
       dispatch({ type: "LOGIN", payload: res.user });
+      // update state
       if (!isCancelled) {
         setIsPending(false);
         setError(null);
@@ -42,5 +34,6 @@ export const useSignup = () => {
   useEffect(() => {
     return () => setIsCancelled(true);
   }, []);
-  return { error, isPending, signup };
+
+  return { error, isPending, login };
 };
